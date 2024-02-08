2e2fa0d1d8ddfa48b2cfeda3fcec5bbb736266aa
marcio <marciopereiraaraujo1997d1@gmail.com>
2024-02-08 11:09:11 -0300

    challenge done :D

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 <a href="https://marcioparaujo.github.io/rocketseat-challenges/">pages</a><br>
 <a href="https://marcioparaujo.github.io/rocketseat-challenges/biscoito-da-sorte/">fortune biscuits</a><br>
-<a href="https://marcioparaujo.github.io/rocketseat-challenges/javascriptDOM/guess-game/">guess game</a>
-<a href="https://marcioparaujo.github.io/rocketseat-challenges/grid/">galaxy</a>
-<a href="https://marcioparaujo.github.io/rocketseat-challenges/landpage-rocket/">landpage rocketseat</a>
-<a href="https://marcioparaujo.github.io/rocketseat-challenges/photos-pages/">photo pages</a>
-<a href="https://marcioparaujo.github.io/rocketseat-challenges/responsivity/">recepy</a>
+<a href="https://marcioparaujo.github.io/rocketseat-challenges/javascriptDOM/guess-game/">guess game</a><br>
+<a href="https://marcioparaujo.github.io/rocketseat-challenges/grid/">galaxy</a><br>
+<a href="https://marcioparaujo.github.io/rocketseat-challenges/landpage-rocket/">landpage rocketseat</a><br>
+<a href="https://marcioparaujo.github.io/rocketseat-challenges/photos-pages/">photo pages</a><br>
+<a href="https://marcioparaujo.github.io/rocketseat-challenges/responsivity/">recepy</a><br>
 
 
---
+++ biscoito-da-sorte/main.js
@@ -0,0 +1,32 @@
+const cookieButton = document.querySelector('#biscuit')
+const closedCookie = document.querySelector('#closed')
+const openedCookie = document.querySelector('#opened')
+const anotherCookie = document.querySelector('#another-biscuit')
+const message = document.querySelector('#message')
+
+const phrases = [
+    'It takes courage to grow up and become who you really are.',
+    'When you can’t find someone to follow, you have to find a way to lead by example.',
+    'The most common way people give up their power is by thinking they don’t have any.',
+    'Vitality shows not only in the ability to persist but in the ability to start over.',
+    'A problem is a chance for you to do your best.',
+    'All our dreams can come true, if we have the courage to pursue them',
+    `Don't sit down and wait for the opportunities to come. Get up and make them.`,
+    'You are never too old to set another goal or to dream a new dream.',
+    `The bad news is time flies. The good news is you're the pilot.`,
+    'Success is not final, failure is not fatal: it is the courage to continue that counts.'
+]
+let randomNumberBetween1And10 = () => Math.round(Math.random()*10)
+const revealTheMessage = () =>{
+    closedCookie.classList.add('hided')
+    openedCookie.classList.remove('hided')
+    let randomArrayPosition = randomNumberBetween1And10()
+    message.innerText = phrases[randomArrayPosition]
+}
+const reset = () =>{
+    closedCookie.classList.toggle('hided')
+    openedCookie.classList.toggle('hided')
+}
+
+cookieButton.addEventListener('click',revealTheMessage)
+anotherCookie.addEventListener('click',reset)
