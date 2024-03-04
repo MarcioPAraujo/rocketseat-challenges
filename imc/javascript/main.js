16b5cbf57276a45f54740de519cc7ebdf159ba16
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2024-03-04 00:06:53 -0300

    number validation on form

--- imc/javascript/main.js
+++ imc/javascript/main.js
@@ -15,7 +15,8 @@ function BMICalc (event){
     const height = (fieldHeight.value)/100
     
     const invalidValue = checkValue(weight) || checkValue(height)
-    if(invalidValue){
+    const invalidNumber = isNumberOutOfRange(weight,height)
+    if(invalidValue || invalidNumber){
         return 'One or both values might be invalid for calculation'
     }
 
@@ -29,6 +30,14 @@ function BMICalc (event){
 }
  
 function checkValue(value){
-    return isNaN(value) || value === ''
-       
+    return isNaN(value) || value === ''     
+}
+function isNumberOutOfRange(weight,height){
+    let tooSmall = weight <= 0 || height <= 0
+    if(tooSmall){
+        return tooSmall
+    }
+
+    let tooBig = weight > 250 || height > 2.20
+    return tooBig
 }
